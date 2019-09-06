1379cb127e68f141fc7565c1d4d809514106e26e
Vassiliy Yegorov <vasyakrg@gmail.com>
2019-09-06 14:43:03 +0700

    fix apps

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db mongo:latest
-docker run -d --network=reddit --network-alias=post vasyakrg/post:1.0
-docker run -d --network=reddit --network-alias=comment vasyakrg/comment:1.0
-docker run -d --network=reddit -p 9292:9292 vasyakrg/ui:1.0
+docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db --name=mongodb mongo:latest
+docker run -d --network=reddit --network-alias=post --name=post vasyakrg/post:logging
+docker run -d --network=reddit --network-alias=comment --name=comment vasyakrg/comment:logging
+docker run -d --network=reddit -p 9292:9292 --name=ui vasyakrg/ui:logging
